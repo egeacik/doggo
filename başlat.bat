dee4727b6ebc3b75963a22483f10ea32d6c8020f
egeacik <65019195+egeacik@users.noreply.github.com>
2021-03-08 11:52:34 +0300

    Add files via upload

---
+++ başlat.bat
@@ -0,0 +1,7 @@
+echo off
+color 0f
+title Dogo bot
+cls
+:a
+node bot.js
+goto a
---
+++ bot.js
@@ -0,0 +1,163 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ disableMentions: 'everyone' });
+const ayarlar = require('./ayarlar.json');
+const fs = require('fs');
+const moment = require('moment');
+const { connect } = require('http2');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.on('ready', () => {
+
+  // Oynuyor Kısmı
+  
+      var actvs = [
+        `${prefix}yardım ${client.guilds.cache.size} sunucuyu`,
+        `${prefix}yardım ${client.users.cache.size} Kullanıcıyı`, 
+        `${prefix}yardım`
+    ];
+    
+    client.user.setActivity(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)], { type: 'LISTENING' });
+    setInterval(() => {
+        client.user.setActivity(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)], { type: 'LISTENING'});
+    }, 15000);
+    
+  
+      console.log ('_________________________________________');
+      console.log (`Kullanıcı İsmi     : ${client.user.username}`);
+      console.log (`Sunucular          : ${client.guilds.cache.size}`);
+      console.log (`Kullanıcılar       : ${client.users.cache.size}`);
+      console.log (`Prefix             : ${ayarlar.prefix}`);
+      console.log (`Durum              : Bot Çevrimiçi!`);
+      console.log ('_________________________________________');
+    
+    });
+
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+client.login(ayarlar.token);
+// 
+const guildInvites = new Map();
+
+client.on("ready", () => {
+  client.guilds.cache.forEach(guild => {
+    guild.fetchInvites()
+    .then(invites => guildInvites.set(guild.id, invites))
+    .catch(err => console.log(err));
+    });
+});
+client.on('inviteCreate', async invite => {
+  guildInvites.set(invite.guild.id, await invite.guild.fetchInvites())
+});
+client.on('guildMemberAdd', async member => {
+  const cachedInvites = guildInvites.get(member.guild.id);
+  const newInvites = await member.guild.fetchInvites();
+  guildInvites.set(member.guild.id, newInvites);
+  try {
+    console.log("Davet Eklendi")
+    const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses);
+    const currentInvites = await db.get(`inv.${usedInvite.inviter.id}.total`)
+    if(currentInvites) {
+      db.set(`inv.${member.id}.inviter`, usedInvite.inviter.id)
+      db.add(`${usedInvite.inviter.id}`, 1)
+    } else {
+      db.set(`inv.${usedInvite.inviter.id}.total`, 1)
+      db.set(`inv.${member.id}.inviter`, usedInvite.inviter.id)
+    }
+  }
+  catch(err) {
+    console.log(err);
+  }
+});
+
+client.on('guildMemberRemove', async member => {
+  const inviter = await db.get(`inv.${member.id}.inviter`)
+  const userinviter = await member.guild.members.fetch(`${inviter}`);
+  const currentInvites = await db.get(`inv.${inviter}.total`)
+  try {
+    console.log("Davet Silindi")
+    db.add(`inv.${inviter}.total`, -1)
+    db.delete(`inv.${member.id}.inviter`)
+  } catch(err) {
+    console.log(err);
+  }
+});
+//
